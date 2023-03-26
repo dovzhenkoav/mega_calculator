ace949a50df041f681e382fb62640685dfbf6f44
Thorinka <mmlentyshenko@gmail.com>
2023-03-26 17:07:28 +0300

    fix bugs in plus(), add tests for plus

--- plus.py
+++ plus.py
@@ -1,14 +1,8 @@
 def plus(user_input):
-    digits = []
     result = 0
     if "+" in user_input:
-        for f in user_input:
-            if f.isdigit():
-                digits.append(f)
-            else:
-                continue
-        for digit in digits:
-            result += int(digit)
+        digits = user_input.split("+")
+        for f in digits:
+            result += int(f)
         return result
-
 
